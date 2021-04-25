cba47229fd7ed95abca9fce0606c6e1974e7aeb0
Andrew Humphries <andrewhumphrieswork@outlook.com>
2021-04-25 15:33:42 -0500

    Added functionality to Set Size button and Reset Board button

---
+++ myScript.js
@@ -0,0 +1,55 @@
+
+
+// Sets number of grid blocks
+function gridNumber(x){
+    for (i=0;i<x;i++){
+        let tag = document.createElement("div");
+        let tic = document.createTextNode(x+1);
+        tag.appendChild(tic);
+        tag.className = "block";
+        document.getElementById("container").appendChild(tag);
+        }
+    }
+
+// Sets number of Columns and Rows
+function gridSize(x){
+
+    document.getElementById("container").style.removeProperty('gridTemplateColumns');
+    document.getElementById("container").style.removeProperty('gridTemplateRows');
+
+    document.getElementById("container").style.gridTemplateColumns = `repeat(${x}, auto)`;
+    document.getElementById("container").style.gridTemplateRows = `repeat(${x}, auto)`;
+    console.log("button pressed")
+}
+
+// initiates board size based on selection
+function setGrid() {
+    let x = document.getElementById("mySelect");
+    let y = parseInt(x.options[x.selectedIndex].text)
+    gridNumber(y*y)
+    gridSize(y)
+  }
+
+//Reset Board to Default Size 4x4
+function resetBoard(){
+    const container = document.getElementById("container")
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+// reset Button Action
+let reset = document.getElementById("reset");
+reset.onclick = function(){
+    resetBoard()
+}
+// Set Size Button Action
+let set = document.getElementsByTagName('input')[0];
+set.onclick = function(){
+    resetBoard()
+    setGrid()
+}
+
+
+gridNumber(4*4)
+gridSize(4)
